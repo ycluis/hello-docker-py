b5f185538da24b7ac3bc768bb8bb3fd6246758cd
ycluis <ycluisdev@gmail.com>
2024-04-28 14:36:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+import json
+
+
 def main():
     print("Hello Docker!")
+
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(f"Current date and time: {dt_string}")
 
 
 if __name__ == "__main__":
     main()
+    print(json.dumps({"complete": 1, "code": 0}))
